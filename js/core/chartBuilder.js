9261c336adea513aaca997ed2d3dd680510ab0c2
emissionscarbonebva-maker <emissionscarbonebva@gmail.com>
2026-03-06 01:33:41 +0100

    Update chartBuilder.js

--- js/core/chartBuilder.js
+++ js/core/chartBuilder.js
@@ -1,1 +1,23 @@
+export function createLineChart(canvasId, labels, datasets){
 
+new Chart(document.getElementById(canvasId),{
+
+type:"line",
+
+data:{labels,datasets},
+
+options:{
+responsive:true,
+interaction:{mode:"index",intersect:false},
+plugins:{
+legend:{position:"top"},
+zoom:{
+pan:{enabled:true,mode:"x"},
+zoom:{wheel:{enabled:true},pinch:{enabled:true},mode:"x"}
+}
+}
+}
+
+});
+
+}
