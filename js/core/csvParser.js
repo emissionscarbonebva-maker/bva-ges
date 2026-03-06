f97ab828c5f9e6053c28262f2b66d5939d7e8cad
emissionscarbonebva-maker <emissionscarbonebva@gmail.com>
2026-03-06 01:33:25 +0100

    Update csvParser.js

--- js/core/csvParser.js
+++ js/core/csvParser.js
@@ -1,1 +1,33 @@
+export function parseCSV(url, callback){
 
+fetch(url)
+.then(r => r.text())
+.then(data => {
+
+let rows = data.split('\n').filter(r => r.trim() !== '');
+
+let sep = rows[0].includes(';') ? ';' : ',';
+
+let headers = rows[0].split(sep);
+
+let result = [];
+
+for(let i=1;i<rows.length;i++){
+
+let cols = rows[i].split(sep);
+
+let obj={};
+
+headers.forEach((h,index)=>{
+obj[h.trim()] = cols[index] ? cols[index].trim() : "";
+});
+
+result.push(obj);
+
+}
+
+callback(result, headers);
+
+});
+
+}
