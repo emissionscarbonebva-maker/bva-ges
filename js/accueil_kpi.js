2cbbd6b49a4ba6f30bbff0e0d42df5e37a4e42f5
emissionscarbonebva-maker <emissionscarbonebva@gmail.com>
2026-03-24 00:41:34 +0100

    Update accueil_kpi.js

--- js/accueil_kpi.js
+++ js/accueil_kpi.js
@@ -643,7 +643,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
             },
 
-            plugins: [pluginAeroLabels, pluginMissingData]
+            plugins: [pluginAeroLabels]
         });
 
     });
